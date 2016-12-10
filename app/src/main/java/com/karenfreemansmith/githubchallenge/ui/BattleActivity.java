23263a7f3b3a294373a615248b43f67f026f704c
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-12-09 18:19:38 -0800

    allow users to logout

--- app/src/main/java/com/karenfreemansmith/githubchallenge/ui/BattleActivity.java
+++ app/src/main/java/com/karenfreemansmith/githubchallenge/ui/BattleActivity.java
@@ -3,8 +3,12 @@ package com.karenfreemansmith.githubchallenge.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.karenfreemansmith.githubchallenge.R;
 
 import butterknife.ButterKnife;
@@ -17,6 +21,23 @@ public class BattleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @OnClick(R.id.buttonStartBattle)
@@ -35,5 +56,13 @@ public class BattleActivity extends AppCompatActivity {
     public void searchPlayer2(View v) {
         Intent intent = new Intent(BattleActivity.this, SearchActivity.class);
         startActivity(intent);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(BattleActivity.this, NewPlayerActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/karenfreemansmith/githubchallenge/ui/BattleResultsActivity.java
+++ app/src/main/java/com/karenfreemansmith/githubchallenge/ui/BattleResultsActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.karenfreemansmith.githubchallenge.R;
 
 import butterknife.ButterKnife;
@@ -27,7 +28,10 @@ public class BattleResultsActivity extends AppCompatActivity {
 
     @OnClick(R.id.buttonLogout)
     public void endGame(View v) {
+        FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(BattleResultsActivity.this, NewPlayerActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
+        finish();
     }
 }
